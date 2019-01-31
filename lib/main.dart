c22a475c9939de1ab519bbef3ad2b7d6e9baae19
golfptw <psgolf16@gmail.com>
2019-01-31 16:25:17 +0700

    Call api show in list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:dio/dio.dart';
+import 'color_loader.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,96 +9,119 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter WTF',
-      home: new RandomWords(),
       theme: new ThemeData(primaryColor: Colors.black),
+      home: new GetAPI(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+// set state
+
+class GetAPI extends StatefulWidget {
   @override
-  RandomWordsState createState() => new RandomWordsState();
+  _GetAPIState createState() => new _GetAPIState();
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final TextStyle _biggerFont = new TextStyle(fontSize: 22);
-  final Set<WordPair> _saved = new Set<WordPair>();
+// working function
 
-  void _pushSaved() {
-    Navigator.of(context)
-        .push(new MaterialPageRoute<void>(builder: (BuildContext context) {
-      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
-        return new ListTile(
-          title: new Text(
-            pair.asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-      });
-      final List<Widget> divied =
-          ListTile.divideTiles(context: context, tiles: tiles).toList();
+class _GetAPIState extends State<GetAPI> {
+  // list color to use in color loader
+  List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.indigo,
+    Colors.pinkAccent,
+    Colors.blue
+  ];
 
-      return new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Saved'),
-        ),
-        body: new ListView(
-          children: divied,
-        ),
-      );
-    }));
+  // use future and Dio to call api and return item
+  Future<List<ItemJSON>> _getItem() async {
+    Response response;
+    response = await Dio()
+        .get('https://www.json-generator.com/api/json/get/cfwZmvEBbC?indent=2');
+    var responseData = response.data;
+    // await new Future.delayed(new Duration(seconds: 2));
+    List<ItemJSON> items = [];
+    for (var user in responseData) {
+      ItemJSON item = ItemJSON(user['index'], user['about'], user['email'],
+          user['name'], user['picture']);
+      items.add(item);
+    }
+    print(items.length);
+    return items;
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('It is header'),
-        actions: <Widget>[
-          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(10),
-      itemBuilder: (BuildContext _context, int i) {
-        if (i.isOdd) {
-          return new Divider();
-        }
-        final int index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(1));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
+    // get item from function and set listview
+    var futureBuilder = FutureBuilder(
+        future: _getItem(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Text('Press button to start.');
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return ColorLoader(colors: colors,duration: Duration(milliseconds: 1200),);
+            case ConnectionState.done:
+              if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(snapshot.data[index].picture),
+                    ),
+                    title: Text(snapshot.data[index].name),
+                    subtitle: Text(snapshot.data[index].email),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) =>
+                                  DetailList(snapshot.data[index])));
+                    },
+                  );
+                },
+              );
           }
+          return null;
         });
-      },
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Get List User'),
+      ),
+      body: Container(
+        child: futureBuilder,
+      ),
     );
   }
+}
+
+// StatelessWidget = new page
+
+class DetailList extends StatelessWidget {
+  final ItemJSON item;
+  DetailList(this.item);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(item.name),
+      ),
+      body: Container(
+        child: Text(item.about),
+      ),
+    );
+  }
+}
+
+// variable in json
+
+class ItemJSON {
+  final int index;
+  final String about, email, name, picture;
+  ItemJSON(this.index, this.about, this.email, this.name, this.picture);
 }
